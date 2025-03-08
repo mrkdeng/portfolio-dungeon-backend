73e3182d3372476db596210f94860be3ff596e9f
Ke Deng <kdeng7.2022@gmail.com>
2025-03-08 09:18:26 -0800

    set host to be public

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ def read_root():
     return {"message": "Hello, FastAPI!"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
